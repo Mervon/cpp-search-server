6d6ce4051a744773c4f060e5aa835036aebb0372
Klimenko Aleksandr <Sanek.0.0.0@mail.ru>
2022-01-17 07:51:47 +0300

    Project_fifth_sprint_first_attempt

---
+++ search-server/read_input_functions.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::string ReadLine();
+
+int ReadLineWithNumber();
---
+++ search-server/request_queue.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <deque>
+#include <string>
+#include <vector>
+
+#include "search_server.h"
+
+class RequestQueue {
+public:
+    explicit RequestQueue(const SearchServer& search_server);
+    
+    template <typename DocumentPredicate>
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate);
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status);
+    std::vector<Document> AddFindRequest(const std::string& raw_query);
+
+    int GetNoResultRequests() const;
+private:
+    const SearchServer& search_server_;
+    struct QueryResult {
+        bool is_empty;
+        std::vector<Document> response;
+        // определите, что должно быть в структуре
+    };
+    std::deque<QueryResult> requests_;
+    const static int min_in_day_ = 1440;
+};
+
+template <typename DocumentPredicate>
+    std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
+        std::vector<Document> result = search_server_.FindTopDocuments(raw_query, document_predicate);
+        bool check = result.empty();
+        if (requests_.size() >= min_in_day_) {
+            requests_.pop_front();
+        }
+        requests_.push_back({check, result});
+        return(result);
+    }
